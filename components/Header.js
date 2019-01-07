dbe89b902d4f5e687ba47a51b1d4feacf15a51ab
Guillaume Gomez <guillaume.gomez846@gmail.com>
2019-01-07 16:44:06 +0100

    change icon

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 import AppBar from '@material-ui/core/AppBar';
-import CameraIcon from '@material-ui/icons/PhotoCamera';
+import RowingIcon from '@material-ui/icons/Rowing';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
@@ -11,7 +11,7 @@ import { styles } from "../style/styles";
 const Header = ({ classes }) => (
   <AppBar position="static" className={classes.appBar}>
     <Toolbar>
-      <CameraIcon className={classes.icon} />
+      <RowingIcon className={classes.icon} />
       <Typography variant="h6" color="inherit" noWrap>
         Json Schema form 101
       </Typography>
