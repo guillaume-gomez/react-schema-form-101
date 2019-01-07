35851229bcda5509c061969b57383fd848cca030
Guillaume Gomez <guillaume.gomez846@gmail.com>
2019-01-07 15:01:04 +0100

    add tiny component to start jsonschema-form-for-material-ui form

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hello world')
+import React from "react";
+import ReactDOM from "react-dom";
+
+class HelloMessage extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello {this.props.name}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <HelloMessage name="Taylor" />,
+  document.getElementById('react-div')
+);
